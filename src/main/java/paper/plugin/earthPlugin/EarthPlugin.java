19ee2d02f71ae2726f30d72283e3fa51ec28b059
codep1ltio <Joonasmikael5@gmail.com>
2026-04-04 00:53:55 +0700

    comment for pan

--- src/main/java/paper/plugin/earthPlugin/EarthPlugin.java
+++ src/main/java/paper/plugin/earthPlugin/EarthPlugin.java
@@ -21,6 +21,17 @@ public final class EarthPlugin extends JavaPlugin implements Listener {
         // ...
     }
 
+    /*
+    * pan to get u started, helpers have the like, functions to make stuff easier, for example console.print()
+    * and you can make ur own helpers, i will refactor them later if it sucks but it shouldnt be that hard
+    * handlers are the things that do the events, so PlayerJoinEventHandle is where you code stuff that
+    * happens after player joins, we of course have to make another file if it gets off topic to like mechanics
+    * i will help u with that, dw and also this file is for only event handlers, and basic stuff, dont code
+    * literal mechanics here, just @EventHandlers like onPlayerJoin event under this comment
+    * also bro u can text me on discord anytime, dont use gpt, only use it to learn or ask something u dont know
+    * dont make it code for u, please i beg u i love human code more
+    */
+
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         console.print(event.getPlayer().getName() + " has joined");
